167e16808fa2950b1a77fbda8de7061cb2bcca1b
ClaireBruce <bobtherainbowface@gmail.com>
2025-06-14 13:36:06 +0100

    Added hint, and loading message

--- src/script.js
+++ src/script.js
@@ -8,11 +8,16 @@ function displayPoem(response) {
 
 function generatePoem(event) {
     event.preventDefault();
-    let instructionsInput = document.querySelector("#user-instruction")
+    let instructionInput = document.querySelector("#user-instruction")
     let apiKey = "03a1od824e34f4b433a55ct759776fa6";
-    let context = "You are a romantic poet who writes short 4 line poems, your mission is to generate a 4 line poem, separate each line with <br />. Make sure to follow the user instructions";
-    let prompt = `Tell me a poem about ${instructionsInput}, please`;
+    let context = "You are a romantic poet who writes short 4 line poems, your mission is to generate a 4 line poem, separate each line with <br />. Always follow the user instructions";
+    let prompt = `Tell me a poem about ${instructionInput.value}, please`;
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+    let poemElement = document.querySelector("#poem");
+    poemElement.classList.remove("hidden");
+    poemElement.innerHTML = `<div class="generating"> ⏳ Generating a poem about ${instructionInput.value}...</div>`;
+
     axios.get(apiUrl).then(displayPoem);
 }
  
